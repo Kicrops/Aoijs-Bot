26bc77388f6be0a0c0d58fa5d03173cfb18df9a2
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-09-27 12:13:23 +0530

    Create status.js

---
+++ handler/status.js
@@ -0,0 +1,33 @@
+/*
+ Status = {
+  text : string | number ,
+  type : "PLAYING" | "playing" | "STREAMING" | "streaming" | "WATCHING" | "watching" | "LISTENING" | "listening" ,
+  time : number ,
+  status? : "online" | "offline" | "idle" | "dnd" ,
+  url? : string ,
+  shardId : number | Array<number> 
+ }
+ returns Array<Status>
+*/
+
+module.exports = [
+
+    {
+
+    text : "Aoijs V5 Template",
+
+    type : "STREAMING",
+
+    time : 13
+
+},
+
+    {
+
+    text : "Made By Ayaka",
+
+    type : "WATCHING",
+
+    time : 13
+
+}]
