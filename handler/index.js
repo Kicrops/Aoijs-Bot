137179b0b1771706f08f1a2e83a9993f48bf62cf
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-11-11 15:17:30 +0530

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -1,5 +1,8 @@
-module.exports = async (Options) => {
+const events = require('./events.js');
 
+module.exports = async (Options) => {
+	events.forEach(x => Options.bot[x]());
+	
   await Options.loader.load(Options.bot.cmd,"../Commands/Bot/");
   await Options.loader.load(Options.voice.cmd,"../Commands/Voice/");
   await Options.loader.load(Options.bot.customFunctions,"../Commands/CustomFunctions/");
