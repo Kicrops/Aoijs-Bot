e720412f338ac00c73aeed49b845b19af41ff4ce
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-11-13 01:49:57 +0530

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -3,9 +3,9 @@ const events = require('./events.js');
 module.exports = async (Options) => {
 	events.forEach(x => Options.bot[x]());
 	
-  await Options.loader.load(Options.bot.cmd,"../Commands/Bot/");
-  await Options.loader.load(Options.voice.cmd,"../Commands/Voice/");
-  await Options.loader.load(Options.bot.customFunctions,"../Commands/CustomFunctions/");
+  await Options.loader.load(Options.bot.cmd,"./Commands/Bot/");
+  await Options.loader.load(Options.voice.cmd,"./Commands/Voice/");
+  await Options.loader.load(Options.bot.customFunctions,"./Commands/CustomFunctions/");
 	
   Options.bot.functionManager.createCustomFunctions( ...( Options.bot.customFunctions.djs.allValues().concat( Options.bot.customFunctions["aoi.js"].allValues() ) ) );
 }
